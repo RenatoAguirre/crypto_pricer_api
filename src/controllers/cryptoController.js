1de3c3fabe92c2834cde02d74d9d8653c0b64a6c
Rena <raaguirre1@miuandes.cl>
2024-03-11 12:16:29 -0300

    fix/fixed currencies endpoint´s response format

--- src/controllers/cryptoController.js
+++ src/controllers/cryptoController.js
@@ -2,7 +2,7 @@ const cryptoService = require('../services/cryptoService')
 
 const getAllCryptos = (req, res) => {
   const cryptos = cryptoService.getAllCryptos()
-  res.status(200).send({ cryptos })
+  res.status(200).send(cryptos)
 }
 
 module.exports = {
